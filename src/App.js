025f3a653ee7ddb26bd94289794d114c474414f9
doublebyte1 <info@doublebyte.net>
2024-01-02 15:54:08 +0000

    - initial import

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { MapContainer, TileLayer, LayersControl} from 'react-leaflet'
+
+
+const center = [51.505, -0.09]
+
+
+const options = {
+  type: 'protobuf',
+  url: 'https://demo.pygeoapi.io/master/collections/lakes/tiles/WorldCRS84Quad/{z}/{x}/{y}?f=mvt',
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <MapContainer center={center} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <LayersControl position="topright">
+          <LayersControl.Overlay name="Vector Tiles on pygeoapi">
+
+          </LayersControl.Overlay>
+        </LayersControl>
+      </MapContainer> 
     </div>
   );
 }
